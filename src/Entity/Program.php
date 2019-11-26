e2c4be84d164add3261c8aa880592f793ed32f19
Sebastien Comberousse <scomberousse@gmail.com>
2019-11-26 13:31:41 +0100

    modif quete symfony doctrine

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -19,11 +19,6 @@ class Program
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $name;
-
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
     private $title;
 
     /**
@@ -39,18 +34,6 @@ class Program
     public function getId(): ?int
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
     }
 
     public function getTitle(): ?string
